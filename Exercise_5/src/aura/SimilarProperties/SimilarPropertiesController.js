3356b06cf96da303dacf898667dde86fd4acc9bb
Greg Rewis <greg@assortedgarbage.com>
2017-02-24 17:17:58 -0800

    Added saveRecord method

--- Exercise_5/src/aura/SimilarProperties/SimilarPropertiesController.js
+++ Exercise_5/src/aura/SimilarProperties/SimilarPropertiesController.js
@@ -1,17 +1,9 @@
 ({
     doInit : function(component, event, helper) {
-        var recId = component.get("v.recordId");
-        var compValue = component.get("v.searchCriteria");
-        var action = component.get("c.getSimilarProperties");
-        action.setParams({
-            recordId: recId,
-            searchCriteria: compValue
-        });
-        action.setCallback(this, function(response){
-            var res = response.getReturnValue();
-            component.set("v.similarProperties", res);
-        });
-        $A.enqueueAction(action);
+        helper.getProps(component);
+    },
+    doSearch : function (component, event, helper) {
+        helper.getProps(component);
     },
     navToRecord : function (component, event, helper) {
         var selectedItem = event.currentTarget;
@@ -24,11 +16,36 @@
     },
     editRecord : function(component, event, helper) {
         var recId = event.getSource().get("v.value");
-        console.log("rec: ", recId)
         var editRecordEvent = $A.get("e.force:editRecord");
         editRecordEvent.setParams({
             "recordId": recId
         });
         editRecordEvent.fire(); 
+    },
+    editMini : function(component, event, helper) {
+        var recId = event.getSource().get("v.value");
+        component.set("v.selectedRecordId", recId);
+        var tempRec = component.find("editRecord");
+        tempRec.set("v.recordId", component.get("v.selectedRecordId"));
+        tempRec.reloadRecord();
+        helper.showHideModal(component);
+    },
+    saveRecord : function(component,event,helper) {
+        var propName = component.find('propName').get("v.value");
+        var propBeds = component.find('propBeds').get("v.value");
+        var propBaths = component.find('propBaths').get("v.value");
+        var propPrice = component.find('propPrice').get("v.value");
+        var propStatus = component.find('propStatus').get("v.value");
+        var tempRec = component.find("editRecord");
+        tempRec.set("v.Name", propName);
+        tempRec.set("v.Name", propBeds);
+        tempRec.set("v.Name", propBaths);
+        tempRec.set("v.Name", propPrice);
+        tempRec.set("v.Name", propStatus);
+        tempRec.saveRecord();
+        helper.showHideModal(component);
+    },
+    closeButton : function(component,event,helper) {
+        helper.showHideModal(component);
     }
 })
