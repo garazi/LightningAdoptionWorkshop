062e99f3679b54c62f3e4ad25da8049d309b3259
Greg Rewis <greg@assortedgarbage.com>
2017-02-24 17:18:28 -0800

    added show/hide modal

--- Exercise_5/src/aura/SimilarProperties/SimilarPropertiesHelper.js
+++ Exercise_5/src/aura/SimilarProperties/SimilarPropertiesHelper.js
@@ -1,6 +1,6 @@
 ({
-	getProps : function(component) {
-		var recId = component.get("v.recordId");
+    getProps : function(component) {
+        var recId = component.get("v.recordId");
         var compValue = component.get("v.searchCriteria");
         var action = component.get("c.getSimilarProperties");
         action.setParams({
@@ -12,5 +12,11 @@
             component.set("v.similarProperties", res);
         });
         $A.enqueueAction(action);
-	}
+    },
+    showHideModal : function(component) {
+        var modal = component.find("editDialog");
+        $A.util.toggleClass(modal, 'slds-hide');
+        var overlay = component.find("overlay");
+        $A.util.toggleClass(overlay, 'slds-hide');
+    }
 })
